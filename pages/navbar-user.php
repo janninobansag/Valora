71211df583f72aedcde71977f3934788f2fab6af
jannino bansag <janninobansag@gmail.com>
2025-09-27 20:49:02 +0800

    valora

---
+++ pages/navbar-user.php
@@ -0,0 +1,12 @@
+<header>
+    <h1 class="logo">Valora</h1>
+    <nav>
+        <a href="#.php">HOME</a>
+        <a href="#">SHOP</a>
+        <a href="#">ABOUT US</a>
+        <a href="#">CONTACT</a>
+    </nav>
+    <div class="auth-buttons">
+        <a href="../process/logout.php" class="btn-signing">Logout</a>
+    </div>
+</header>
